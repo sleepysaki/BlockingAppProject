24330d3bbeb82e6bc4d188b9aca2d9ec04d0e78a
sleepysaki <caotrankhanhlinh35@gmail.com>
2025-12-05 06:48:44 +0700

    restructure config file

--- composeApp/src/commonMain/kotlin/com/usth/blockingappproject/config/AppConfig.kt
+++ composeApp/src/commonMain/kotlin/com/usth/blockingappproject/config/AppConfig.kt
@@ -1,6 +1,10 @@
 package com.usth.blockingappproject.config
 
-// Define the expectation
+// Define the expectation (mechanism expect/actual of KPM)
+// Handle platform divergence
+
+// Expect a variable called BaseUrl to exist
+// Define actual value in androidMain and webMain
 expect object AppConfig {
     val BASE_URL: String
 }
